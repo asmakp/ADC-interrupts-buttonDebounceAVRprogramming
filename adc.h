0d78bb6a343f55de41a1a94ff699fe454d7e6990
asmapk <asma.k.patel1@gmail.com>
2021-11-05 15:26:24 +0100

    DELUPPGIFT 3: LÄS SPÄNNING MED HJÄLP AV ADCN Two files added adc.c and adc.h

---
+++ adc.h
@@ -0,0 +1,10 @@
+#ifndef _ADC_H_
+#define _ADC_H_
+
+//uppgift 3 variables
+#define LED_PIN_PWM 6     //pin 6 on portD
+#define analog_read 0     //pin A0 on portC
+
+void ADC_init(void);
+
+#endif // _ADC_H_
--- main.c
+++ main.c
@@ -6,24 +6,42 @@
 #include "led.h"
 #include "serial.h"
 #include "timer.h"
+#include "adc.h"
 
 //DELUPPGIFT 2: LÄS AV DIGITALINGÅNG
-#define KeyRead 0                     //pin 8 from PORTB
-int stateChange = 0;                 // variable to store the statechange of the switch
-volatile static int flag = 0;
+//#define KeyRead 0                     //pin 8 from PORTB
+//int stateChange = 0;                 // variable to store the statechange of the switch
+//volatile static int flag = 0;
+
+
+
+//DELUPPGIFT 3: Read voltage with help of ADC
+volatile int flag = 0;
+volatile uint8_t pot_val = 0; ///To read the value from the data register the MSB bits ADCH
+//void ADC_init(void);
 
 
 void main (void) {
-	
-    sei();                   //globaly enables interrupts
-	DDRB |= (1<< KeyRead);  //set switch pin 8 as output
-	uart_init();
-	timer_init();
+
+	sei(); //globaly enables interrupts
+
+
+	//Uppgift 3 .Potentiometer
+	DDRD |= (1<<LED_PIN_PWM);   
+	DDRC &= ~(1<<analog_read); //analog pin A0 set as input
+	ADC_init();
+	timer2_init();
+
+    
+	//Uppgift 2 switch
+	//DDRB |= (1<< KeyRead);  //set switch pin 8 as output
+	//uart_init();
+	//timer_init();
 
     	
 	while (1) {
 		
-	  if(flag)
+	 /* if(flag)
 		   {
 			if((PINB & (1<<KeyRead)) == 1  && stateChange == 0){ //If switch is pressed enter loop
 					stateChange = 1;
@@ -39,17 +57,34 @@ void main (void) {
 					   stateChange = 0;
 					   flag = 0;
 			       }
-	       }
+	       }*/
     } 
+}
+
+//Interrupt for Uppgift 3
+
+ISR(ADC_vect)
+
+{
+	
+	pot_val = ADCH; //Register storing MSB bits (8 bits)
+}
+
+ISR(TIMER2_COMPA_vect){
+	
+	ADCSRA  |= (1<< ADSC ) ; //start conversion
+	
+    OCR0A = pot_val;  // store potval in time compare register
+	
 }
 
 //Interrupt for Uppgift 2
 
-ISR (TIMER0_COMPA_vect){    //Interrupt occours on time compare match .i.e., counting up till 156
+/*ISR (TIMER0_COMPA_vect){    //Interrupt occours on time compare match .i.e., counting up till 156
 
 	flag = 1;               //set flag.     
 
-}
+}*/
 
 
 	
