0d2cbe8b5087d1a64fbac7a54f79e006cb2bfc77
asmapk <asma.k.patel1@gmail.com>
2021-11-05 19:56:08 +0100

    changes made DELUPPGIFT 2: LÄS AV DIGITALINGÅNG changes made for variable names

--- adc.c
+++ adc.c
@@ -2,34 +2,33 @@
 
 #include "adc.h"
 
+void ADC_init()
+{
+    //  DELUPPGIFT 3: Read voltage with help of ADC
+    //ADC Control and Status Register A
+    //ADEN: ADC Enable Writing this bit to one enables the ADC.
+    //ADSC : ADC Start Conversion In Single Conversion mode, write this bit to one to start each conversion.
+    //ADIF: ADC Interrupt Flag This bit is set when an ADC conversion completes and the Data Registers are updated.
+    //ADIE: ADC Interrupt Enable When this bit is written to one , the ADC Conversion Complete Interrupt is activated.
+    //ADPSn: ADC Prescaler Select,the 2 bits determine the division factor between the system clock frequency
+    //and the input clock to the ADC. ( 0 1 1  = prescalar 8)
 
+    ADCSRA = (1 << ADEN) | (1 << ADIE);
+    //ADCSRA |=(1<< ADIF ) ; have a little doubt this
+    ADCSRA |= (1 << ADSC);
 
-void ADC_init(){
-	//  DELUPPGIFT 3: Read voltage with help of ADC
-               //ADC Control and Status Register A
-               //ADEN: ADC Enable Writing this bit to one enables the ADC.
-               //ADSC : ADC Start Conversion In Single Conversion mode, write this bit to one to start each conversion.
-			   //ADIF: ADC Interrupt Flag This bit is set when an ADC conversion completes and the Data Registers are updated.
-			   //ADIE: ADC Interrupt Enable When this bit is written to one , the ADC Conversion Complete Interrupt is activated.
-			   //ADPSn: ADC Prescaler Select,the 2 bits determine the division factor between the system clock frequency
-			   //and the input clock to the ADC. ( 0 1 1  = prescalar 8)
-			   
-               ADCSRA =  (1<<ADEN )|  (1<< ADIE ); 
-			   //ADCSRA |=(1<< ADIF ) ; have a little doubt this
-               ADCSRA  |= (1<< ADSC ) ;
+    ADCSRA &= ~(1 << ADPS2);
+    ADCSRA |= (1 << ADPS1) | (1 << ADPS0);
 
-			   ADCSRA &= ~( 1<<ADPS2 );
-			   ADCSRA  |= (1<<ADPS1 )|(1<<ADPS0 );  
-            
-               DIDR0 = ( 1<< ADC0D ); //Digital Input Disable Register 0 set to logic one, the buffer on the corresponding ADC pin is disabled.
+    DIDR0 = (1 << ADC0D); //Digital Input Disable Register 0 set to logic one, the buffer on the corresponding ADC pin is disabled.
 
-               ADMUX = (1<< REFS0 );  //REFSn: Reference Selection,These bits select the voltage reference for the ADC.
-			   ADMUX &= ~(1<< REFS1 ); // we select AVcc the supply voltage 5V.
+    ADMUX = (1 << REFS0);   //REFSn: Reference Selection,These bits select the voltage reference for the ADC.
+    ADMUX &= ~(1 << REFS1); // we select AVcc the supply voltage 5V.
 
-			   ADMUX  |= (1<<ADLAR );       //ADLAR: ADC Left Adjust - to left adjust the result the ADC Data Register.   
-			                                 //Reading 8  MSB bit
-			   ADMUX &= ~(1<<MUX3 );      //TO select the analog pin ADC0  
-			   ADMUX &= ~(1<<MUX2 );
-			   ADMUX &= ~(1<<MUX1 );
-			   ADMUX &= ~(1<<MUX0 );   
+    ADMUX |= (1 << ADLAR); //ADLAR: ADC Left Adjust - to left adjust the result the ADC Data Register.
+                           //Reading 8  MSB bit
+    ADMUX &= ~(1 << MUX3); //TO select the analog pin ADC0
+    ADMUX &= ~(1 << MUX2);
+    ADMUX &= ~(1 << MUX1);
+    ADMUX &= ~(1 << MUX0);
 }
--- main.c
+++ main.c
@@ -9,60 +9,58 @@
 #include "adc.h"
 
 //DELUPPGIFT 2: LÄS AV DIGITALINGÅNG
-//#define KeyRead 0                     //pin 8 from PORTB
-//int stateChange = 0;                 // variable to store the statechange of the switch
-//volatile static int flag = 0;
-
-
+#define Key1 0		 //pin 8 from PORTB
+int CurrentState = 0; // variable to store the statechange of the switch
+int PreviousState = 0;
+volatile static int flag = 0;
 
 //DELUPPGIFT 3: Read voltage with help of ADC
-volatile int flag = 0;
-volatile uint8_t pot_val = 0; ///To read the value from the data register the MSB bits ADCH
+//volatile int flag = 0;
+//volatile uint8_t pot_val = 0; ///To read the value from the data register the MSB bits ADCH
 //void ADC_init(void);
 
-
-void main (void) {
+void main(void)
+{
 
 	sei(); //globaly enables interrupts
 
-
 	//Uppgift 3 .Potentiometer
-	DDRD |= (1<<LED_PIN_PWM);   
-	DDRC &= ~(1<<analog_read); //analog pin A0 set as input
-	ADC_init();
-	timer2_init();
+	//DDRD |= (1<<LED_PIN_PWM);
+	//DDRC &= ~(1<<analog_read); //analog pin A0 set as input
+	//ADC_init();
+	//timer2_init();
 
-    
 	//Uppgift 2 switch
-	//DDRB |= (1<< KeyRead);  //set switch pin 8 as output
-	//uart_init();
-	//timer_init();
+	DDRB |= (1 << Key1); //set switch pin 8 as output
+	uart_init();
+	timer_init();
 
-    	
-	while (1) {
-		
-	 /* if(flag)
-		   {
-			if((PINB & (1<<KeyRead)) == 1  && stateChange == 0){ //If switch is pressed enter loop
-					stateChange = 1;
-					printf_P(PSTR("pushed\r\n"));
-					flag = 0;
+	while (1)
+	{
+
+		//Uppgift 2 switch
+
+		if (flag)
+		{
+			if ((PINB & (1 << Key1)) == 1 && PreviousState == 0) //If switch is pressed enter loop 
+			{ 
+				PreviousState = 1;
+				CurrentState = 0;
+				printf_P(PSTR("pushed\r\n"));
 			}
-			else  if((PINB & (1<<KeyRead)) == 1  && stateChange == 1) {
-				 flag = 0;
-                      
-				}
-				else if((PINB & (1<<KeyRead)) == 0  && stateChange == 1){
-				 printf_P(PSTR("released\r\n"));
-					   stateChange = 0;
-					   flag = 0;
-			       }
-	       }*/
-    } 
+
+			else if ((PINB & (1 << Key1)) == 0 && (PreviousState != CurrentState))
+			{
+				printf_P(PSTR("released\r\n"));
+				PreviousState = 0;
+			}
+			flag = 0;
+		}
+	}
 }
 
 //Interrupt for Uppgift 3
-
+/*
 ISR(ADC_vect)
 
 {
@@ -76,15 +74,12 @@ ISR(TIMER2_COMPA_vect){
 	
     OCR0A = pot_val;  // store potval in time compare register
 	
-}
+}*/
 
 //Interrupt for Uppgift 2
 
-/*ISR (TIMER0_COMPA_vect){    //Interrupt occours on time compare match .i.e., counting up till 156
+ISR(TIMER0_COMPA_vect)
+{ //Interrupt occours on time compare match .i.e., counting up till 156
 
-	flag = 1;               //set flag.     
-
-}*/
-
-
-	
+	flag = 1; //set flag.
+}
